17bc3f5f1b64b38ee0eedd0b08cfb0c1ac25954a
Nathan <nathan@inspire9.com.au>
2010-01-28 13:03:27 +1100

    its a package

--- sqlite.rb
+++ sqlite.rb
@@ -1,11 +1,15 @@
 
+dep 'sqlite' do
+  requires 'sqlite gem'
+end
+
 gem 'sqlite gem' do
   requires 'sqlite software'
   installs 'sqlite3-ruby'
   provides []
 end
 
-dep 'sqlite software'
+pkg 'sqlite software'
   installs {
     via :apt, %w[libsqlite3-ruby libsqlite3-dev]
     via :brew, 'sqlite'
