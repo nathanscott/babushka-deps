d65f4460fdece79ce0930b3abf1cab9004680add
nathanscott <nathan@inspire9.com.au>
2010-02-13 17:34:06 +1100

    re sphinxed

--- sphinx.rb
+++ sphinx.rb
@@ -1,6 +1,6 @@
 src 'sphinx' do
   requires 'build tools'
-  provides 'sphinx'
+  provides 'search'
   prefix '/opt/local'
   configure_args '--with-pgsql'
   source "http://www.sphinxsearch.com/downloads/sphinx-0.9.9.tar.gz"
